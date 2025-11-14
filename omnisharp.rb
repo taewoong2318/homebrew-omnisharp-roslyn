f2d67394bb23d78dbc1580643bc1319a7fe063d3
Taewoong Han <taewoong@taewoong.me>
2025-11-15 01:33:17 +0900

    Update OmniSharp to v1.39.15

--- omnisharp.rb
+++ omnisharp.rb
@@ -1,29 +1,29 @@
 class Omnisharp < Formula
   desc "Cross platform .NET development in the editor of your choice"
   homepage "https://www.omnisharp.net/"
-  version "1.39.14"
+  version "1.39.15"
   license "MIT"
 
   depends_on "dotnet"
 
   if OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/OmniSharp/omnisharp-roslyn/releases/download/v1.39.14/omnisharp-osx-arm64-net6.0.zip"
-    sha256 "c3a345e07e6eda5c6adf9d4fc067640dabe0c1a75c2cac49340aec21f1687799"
+    url "https://github.com/OmniSharp/omnisharp-roslyn/releases/download/v1.39.15/omnisharp-osx-arm64-net6.0.zip"
+    sha256 "678be5bb972d04bbf5e1426e5e7562261e176fa781784d0f13877d8c4391ec3e"
   end
 
   if OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/OmniSharp/omnisharp-roslyn/releases/download/v1.39.14/omnisharp-osx-x64-net6.0.zip"
-    sha256 "c1dd5fb6a45420b55b1698f1a49da056b643c834d57004bc65ed32ee9335f308"
+    url "https://github.com/OmniSharp/omnisharp-roslyn/releases/download/v1.39.15/omnisharp-osx-x64-net6.0.zip"
+    sha256 "6ac1f8b1dfb1e4515f61d120f2cb5ab8404134ec62c441e4ab70ef30e0ac6d07"
   end
 
   if OS.linux? && Hardware::CPU.arm?
-    url "https://github.com/OmniSharp/omnisharp-roslyn/releases/download/v1.39.14/omnisharp-linux-arm64-net6.0.zip"
-    sha256 "0e2c69f191268959d68a35e1cc8fd832a688af516a9945ef25dc8c6ea014a7c2"
+    url "https://github.com/OmniSharp/omnisharp-roslyn/releases/download/v1.39.15/omnisharp-linux-arm64-net6.0.zip"
+    sha256 "376e2007b9a828e7ef58aac1c3397723e0060c8251f29a86b0d8d6747c000e85"
   end
 
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/OmniSharp/omnisharp-roslyn/releases/download/v1.39.14/omnisharp-linux-x64-net6.0.zip"
-    sha256 "eba3f00a807517789be0188f86f8b685d74359d6bd684d7a0b0125e33ad59d89"
+    url "https://github.com/OmniSharp/omnisharp-roslyn/releases/download/v1.39.15/omnisharp-linux-x64-net6.0.zip"
+    sha256 "41838cd21abbf01d7ab41fd53d19297a6f3ec5a07bb75ec139b0ead4195d1239"
   end
 
   def install
